f82df2171a4182758c347dc824d0d4e006964b92
wdawadwda <wdawadwdawork@gmail.com>
2023-04-14 23:34:53 +0300

    feat:
    hw35: completed
    hw:36 task 1-5

---
+++ src/hw35/main.ts
@@ -0,0 +1,96 @@
+//! HW 35
+//!_______________________________________________________________________________________
+
+type User = {
+name: string,
+phone: string,
+email: string,
+animals?: string[],
+cars?: Array<string>,
+hasChildren: boolean,
+hasEducation: boolean,
+}
+
+type Users = User[]
+
+const users: Users = [
+{
+    name: "Harry Felton",
+    phone: "(09) 897 33 33",
+    email: "felton@gmail.com",
+    animals: ["cat"],
+    cars: ["bmw"],
+    hasChildren: false,
+    hasEducation: true
+    
+},
+{
+    name: "May Sender",
+    phone: "(09) 117 33 33",
+    email: "sender22@gmail.com",
+    hasChildren: true,
+    hasEducation: true
+},
+{
+    name: "Henry Ford",
+    phone: "(09) 999 93 23",
+    email: "ford0@gmail.com",
+    cars: ["bmw", "audi"],
+    hasChildren: true,
+    hasEducation: false
+}
+]
+
+// Task 1
+//___________________________________________________________________________________
+
+function nameString(users:Users):string{
+    return users.reduce((accumulator, user, index) => {
+        const separator = index === users.length - 1 ? '' : ', ';
+        return accumulator + (user.name ? user.name + separator : '')
+    },'')
+}
+
+console.log(nameString(users))
+
+// Task 2
+//___________________________________________________________________________________
+
+function totalCars(users:Users):number{
+    return users.reduce((accumulator, user) => {
+        return accumulator + (user.cars ? user.cars.length : 0);
+    }, 0)
+}
+
+console.log(totalCars(users))
+
+// Task 3
+//___________________________________________________________________________________
+
+function isEducation(users:Users):Array<User>{
+    return users.filter((user) => {
+        return user.hasEducation === true
+    })
+}
+
+console.log(isEducation(users))
+
+// Task 4
+//___________________________________________________________________________________
+
+function isanimals(users:Users):User[]{
+    return users.filter((user) =>{
+        return user.animals !== undefined
+    })
+}
+
+console.log(isanimals(users))
+
+// Task 5
+//___________________________________________________________________________________
+
+function carBrand(users:Users):string{
+    return users.flatMap(user => user.cars ?? []).join(", ");
+}
+
+console.log(carBrand(users))
